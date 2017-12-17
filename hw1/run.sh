458b840e88d53eb32d36ec599f6e2aadb630beeb
dmitri nesterenko <dmitri.nesterenko@gmail.com>
2017-12-17 00:10:34 -0500

    Fix the name of the expert to use

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cmd="python3 -m pdb ./run_expert.py --max_timesteps 1000 --num_rollouts 20 experts/RoboschoolHopper-v1.pkl RoboschoolHopper-v1"
+cmd="python3 -m pdb ./run_expert.py --max_timesteps 1000 --num_rollouts 20 experts/RoboschoolHopper-v1 RoboschoolHopper-v1"
 #nohup $cmd & #> output/output.out 2>&1 < /dev/null &
 $cmd
